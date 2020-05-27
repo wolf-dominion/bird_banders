edca4efa88c5b2465d6a1cbb24c2d84fb316cfde
wolf-dominion <hannah_drury@yahoo.com>
2020-05-27 10:04:18 -0600

    Added actions in birds and birdcaptures controllers

--- app/controllers/bird_captures_controller.rb
+++ app/controllers/bird_captures_controller.rb
@@ -1,2 +1,38 @@
 class BirdCapturesController < ApplicationController
+    before_action :get_capture_by_id, only: [:show, :update, :destroy]
+    def index
+        @birdcaptures = BirdCapture.all
+        tagged_hash = {"Bird_captures" => @birdcaptures}
+        render json: tagged_hash
+    end
+
+    def show
+        render json: @birdcapture
+    end
+
+    def create
+        @birdcapture = BirdCapture.create(birdcapture_params)
+        render json: @birdcapture
+    end
+
+    def update
+        @birdcapture.update(birdcapture_params)
+        render json: @birdcapture
+    end
+
+    # def destroy
+    #     @capture.destroy
+    #     redirect_to action: "index"
+    # end
+
+    private
+
+    def birdcapture_params
+        params.require(:birdCapture).permit(:bird_id, :capture_id, :user_id).reject { |_, v| v.blank? }
+    end
+
+    def get_birdcapture_by_id
+        @birdcapture = BirdCapture.find(params[:id])
+    end
+
 end
--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,2 +1,38 @@
 class BirdsController < ApplicationController
+    before_action :get_bird_by_id, only: [:show, :update, :destroy]
+    def index
+        @birds = Bird.all
+        tagged_hash = {"Birds" => @birds}
+        render json: tagged_hash
+    end
+
+    def show
+        render json: @bird
+    end
+
+    def create
+        @bird = Bird.create(bird_params)
+        render json: @bird
+    end
+
+    def update
+        @bird.update(bird_params)
+        render json: @bird
+    end
+
+    # def destroy
+    #     @capture.destroy
+    #     redirect_to action: "index"
+    # end
+
+    private
+
+    def bird_params
+        params.require(:bird).permit(:bandId, :species).reject { |_, v| v.blank? }
+    end
+
+    def get_bird_by_id
+        @bird = Bird.find(params[:id])
+    end
+
 end
